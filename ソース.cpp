9752a1248578642054a5204218e51db9f036dcbf
Satori32 <Huga8989+githubf@gmail.com>
2024-11-09 00:29:04 +0900

    プロジェクト ファイルを追加します。

---
+++ ソース.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+#include "Vector.h"
+#include "FixedVector.h"
+struct Type{
+	//type Type = NULL;
+	typedef void* Type;
+	//type T = Type;
+};
+
+struct TypeAndValue {
+	Type T;
+	T.T Value;
+};
+
+struct ConstHolder {
+	//type Type = NULL;
+	typedef void* Type;
+	//type T = Type;
+	const T Value;
+};
+
+template<size_t N>
+ConstHolder ConstructConstHolder<N>() {
+	ConstHolder C = { N };
+
+	return C;
+}
+
+struct GraphSystem {
+	//type Type = NULL;
+	typedef void* Type;
+	//type T = Type;
+
+	T Value;
+
+	TypeAndValue Root;
+};
+struct GraphParam {//plz rewrite this.
+
+	void* Arrow[16];
+	void* Weight = 0xdeadbeef;
+};
+struct GraphNode {
+	//type Type = NULL;
+	typedef void* Type;
+	//type T = Type;
+
+	T Value;
+	GraphParam P;
+	Vector<TypeAndValue> Node;
+};
+struct FixedGraphNode {
+	//type Type = NULL;
+	typedef void* Type;
+	//type T = Type;
+
+	ConstHolder C;
+
+	T Value;
+	GraphParam P;
+	FixedVector<TypeAndValue,C.Value> Node;
+};
+
+struct EndPoint{
+	//type Type = NULL;
+	typedef void* Type;
+	//type T = Type;
+	
+	T Value;
+	GraphParam P;
+};
